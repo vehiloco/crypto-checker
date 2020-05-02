efcdde18a64ea3432d741ad8a83c54c186673cba
xingweitian <xingweitian@gmail.com>
2020-05-02 00:39:02 -0400

    Update.

--- scripts/dependency-build.sh
+++ scripts/dependency-build.sh
@@ -5,15 +5,13 @@ set -e
 export JSR308=$(cd $(dirname "$0")/../../ && pwd)
 export CHECKERFRAMEWORK=${JSR308}/checker-framework
 
-export REPO_SITE="${REPO_SITE:-opprop}"
-
-echo "------ Downloading everything from REPO_SITE: $REPO_SITE ------"
-
 if [[ -d ${CHECKERFRAMEWORK} ]] ; then
     (cd ${CHECKERFRAMEWORK} && git pull)
 else
     BRANCH=PropertyFileHandler
-    (cd ${JSR308} && git clone -b ${BRANCH} --depth 99 https://github.com/"$REPO_SITE"/checker-framework.git)
+    ORGANIZATION=opprop
+    echo "------ Downloading checker-framework from ORGANIZATION: $ORGANIZATION, BRANCH: $BRANCH ------"
+    (cd ${JSR308} && git clone -b ${BRANCH} https://github.com/"$ORGANIZATION"/checker-framework.git)
 fi
 
 (cd ${CHECKERFRAMEWORK} && source checker/bin-devel/build.sh)
