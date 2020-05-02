059d9c7f1013066b309e1f4d6dde97189aacdcc8
xingweitian <xingweitian@gmail.com>
2020-05-02 02:59:08 -0400

    Update.

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,6 +12,6 @@ RUN_CORPUS_SCRIPT_PATH="$CRYPTO_CHECKER_PATH/scripts/run-corpus.py"
 EXECUTABLE_PATH="$CRYPTO_CHECKER_PATH/scripts/run-dljc.sh"
 
 # Build the crypto-checker
-cd ${CRYPTO_CHECKER_PATH} && ./gradlew assemble &&./gradlew publishToMavenLocal
+cd ${CRYPTO_CHECKER_PATH} && ./gradlew assemble && ./gradlew copyDependencies &&./gradlew publishToMavenLocal
 
 python2 ${RUN_CORPUS_SCRIPT_PATH} --corpus-file ${CORPUS_FILE_PATH}  --executable ${EXECUTABLE_PATH}
