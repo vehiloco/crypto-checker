34288b04b0b2bf3b55d3f3505fe3e45dbce134b4
Alex Liu <alexliusch@gmail.com>
2022-03-28 19:33:11 -0400

    Use SubtypeIsSubsetQualifierHierarchy and add GLB and LUB test cases (#13)

--- src/main/java/org/checkerframework/checker/crypto/CryptoAnnotatedTypeFactory.java
+++ src/main/java/org/checkerframework/checker/crypto/CryptoAnnotatedTypeFactory.java
@@ -2,33 +2,12 @@ package org.checkerframework.checker.crypto;
 
 import java.lang.annotation.Annotation;
 import java.util.Collection;
-import java.util.List;
-import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.util.Elements;
-import org.checkerframework.checker.crypto.qual.AllowedAlgorithms;
-import org.checkerframework.checker.crypto.qual.AllowedProviders;
-import org.checkerframework.checker.crypto.qual.Bottom;
-import org.checkerframework.checker.crypto.qual.UnknownAlgorithmOrProvider;
 import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
 import org.checkerframework.common.basetype.BaseTypeChecker;
-import org.checkerframework.framework.type.ElementQualifierHierarchy;
 import org.checkerframework.framework.type.QualifierHierarchy;
-import org.checkerframework.javacutil.AnnotationBuilder;
-import org.checkerframework.javacutil.AnnotationUtils;
+import org.checkerframework.framework.type.SubtypeIsSubsetQualifierHierarchy;
 
 public class CryptoAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
-
-    /** The @{@link AllowedAlgorithms} annotation. */
-    protected final AnnotationMirror ALLOWEDALGORITHMS =
-            AnnotationBuilder.fromClass(elements, AllowedAlgorithms.class);
-    /** The @{@link AllowedProviders} annotation. */
-    protected final AnnotationMirror ALLOWPROVIDERS =
-            AnnotationBuilder.fromClass(elements, AllowedProviders.class);
-    /** The @{@link Bottom} annotation. */
-    protected final AnnotationMirror BOTTOM = AnnotationBuilder.fromClass(elements, Bottom.class);
-    /** The @{@link UnknownAlgorithmOrProvider} annotation. */
-    protected final AnnotationMirror UNKNOWNALGORITHMORPROVIDER =
-            AnnotationBuilder.fromClass(elements, UnknownAlgorithmOrProvider.class);
 
     public CryptoAnnotatedTypeFactory(BaseTypeChecker checker) {
         super(checker);
@@ -37,58 +16,18 @@ public class CryptoAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
 
     @Override
     protected QualifierHierarchy createQualifierHierarchy() {
-        return new CryptoQualifierHierarchy(this.getSupportedTypeQualifiers(), elements);
+        return new CryptoQualifierHierarchy(this.getSupportedTypeQualifiers());
     }
 
-    private final class CryptoQualifierHierarchy extends ElementQualifierHierarchy {
+    private final class CryptoQualifierHierarchy extends SubtypeIsSubsetQualifierHierarchy {
 
         /**
          * Creates a CryptoQualifierHierarchy from the given classes.
          *
          * @param qualifierClasses classes of annotations that are the qualifiers for this hierarchy
-         * @param elements element utils
          */
-        public CryptoQualifierHierarchy(
-                Collection<Class<? extends Annotation>> qualifierClasses, Elements elements) {
-            super(qualifierClasses, elements);
-        }
-
-        @Override
-        public boolean isSubtype(final AnnotationMirror subtype, final AnnotationMirror supertype) {
-            if (AnnotationUtils.areSameByName(supertype, UNKNOWNALGORITHMORPROVIDER)
-                    || AnnotationUtils.areSameByName(subtype, BOTTOM)) {
-                return true;
-            } else if (AnnotationUtils.areSameByName(subtype, UNKNOWNALGORITHMORPROVIDER)
-                    || AnnotationUtils.areSameByName(supertype, BOTTOM)) {
-                return false;
-            } else if (AnnotationUtils.areSameByName(subtype, ALLOWEDALGORITHMS)
-                    && AnnotationUtils.areSameByName(supertype, ALLOWEDALGORITHMS)) {
-                return compareAllowedAlgorithmOrProviderTypes(subtype, supertype);
-            } else if (AnnotationUtils.areSameByName(subtype, ALLOWPROVIDERS)
-                    && AnnotationUtils.areSameByName(supertype, ALLOWPROVIDERS)) {
-                return compareAllowedAlgorithmOrProviderTypes(subtype, supertype);
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public AnnotationMirror greatestLowerBound(AnnotationMirror a1, AnnotationMirror a2) {
-            return CryptoAnnotatedTypeFactory.this.BOTTOM;
-        }
-
-        @Override
-        public AnnotationMirror leastUpperBound(AnnotationMirror a1, AnnotationMirror a2) {
-            return CryptoAnnotatedTypeFactory.this.UNKNOWNALGORITHMORPROVIDER;
-        }
-
-        private boolean compareAllowedAlgorithmOrProviderTypes(
-                final AnnotationMirror subtype, final AnnotationMirror supertype) {
-            List<String> supertypeRegexList =
-                    AnnotationUtils.getElementValueArray(supertype, "value", String.class, true);
-            List<String> subtypeRegexList =
-                    AnnotationUtils.getElementValueArray(subtype, "value", String.class, true);
-            return supertypeRegexList.containsAll(subtypeRegexList);
+        public CryptoQualifierHierarchy(Collection<Class<? extends Annotation>> qualifierClasses) {
+            super(qualifierClasses, processingEnv);
         }
     }
 }
---
+++ tests/general/GlbLubTest.java
@@ -0,0 +1,112 @@
+package general;
+
+import org.checkerframework.checker.crypto.qual.AllowedAlgorithms;
+import org.checkerframework.checker.crypto.qual.AllowedProviders;
+import org.checkerframework.checker.crypto.qual.Bottom;
+import org.checkerframework.checker.crypto.qual.UnknownAlgorithmOrProvider;
+
+class GlbLubTest {
+
+    void testLub1(
+            @AllowedAlgorithms({"algorithm1"}) String algo,
+            @AllowedProviders({"provider1"}) String prov) {
+
+        String either;
+        if (true) {
+            either = algo;
+        } else {
+            either = prov;
+        }
+        // :: error: (assignment.type.incompatible)
+        algo = either;
+        // :: error: (assignment.type.incompatible)
+        prov = either;
+    }
+
+    void testLub2(
+            @AllowedAlgorithms({"algorithm1", "algorithm2"}) String algo1,
+            @AllowedAlgorithms({"algorithm1", "algorithm3"}) String algo2) {
+
+        String either;
+        if (true) {
+            either = algo1;
+        } else {
+            either = algo2;
+        }
+        // :: error: (assignment.type.incompatible)
+        algo1 = either;
+        // :: error: (assignment.type.incompatible)
+        algo2 = either;
+        @AllowedAlgorithms({"algorithm1", "algorithm2", "algorithm3"})
+        String algo3 = either;
+    }
+
+    void testLub3(
+            @AllowedProviders({"provider1", "provider2"}) String prov1,
+            @AllowedProviders({"provider1", "provider3"}) String prov2) {
+
+        String either;
+        if (true) {
+            either = prov1;
+        } else {
+            either = prov2;
+        }
+        // :: error: (assignment.type.incompatible)
+        prov1 = either;
+        // :: error: (assignment.type.incompatible)
+        prov2 = either;
+        @AllowedProviders({"provider1", "provider2", "provider3"})
+        String prov3 = either;
+    }
+
+    public class MyClass1<T extends @AllowedAlgorithms({"a", "b", "c", "d"}) String> {
+        public T key;
+    }
+
+    void testGlb1() {
+        // ::error: (assignment.type.incompatible)
+        MyClass1<? extends @AllowedAlgorithms({"a", "c", "e"}) String> f1 = new MyClass1<>();
+        @AllowedAlgorithms({"a", "c"})
+        String x = f1.key;
+        @AllowedAlgorithms({"a", "e"})
+        // ::error: (assignment.type.incompatible)
+        String y = f1.key;
+    }
+
+    public class MyClass2<T extends @Bottom String> {
+        public T key;
+    }
+
+    void testGlb2() {
+        // ::error: (assignment.type.incompatible)
+        MyClass2<? extends @AllowedAlgorithms("a") String> f2 = new MyClass2<>();
+        @AllowedAlgorithms({"e"})
+        String x = f2.key;
+    }
+
+    public class MyClass3<T extends @AllowedProviders("a") String> {
+        public T key;
+    }
+
+    void testGlb3() {
+        // ::error: (assignment.type.incompatible)
+        MyClass3<? extends @AllowedAlgorithms("a") String> f3 = new MyClass3<>();
+        @AllowedAlgorithms({"b"})
+        String x = f3.key;
+        @AllowedProviders({"b"})
+        String y = f3.key;
+    }
+
+    public class MyClass4<T extends @AllowedAlgorithms("a") String> {
+        public T key;
+    }
+
+    void testGlb4() {
+        MyClass4<? extends @UnknownAlgorithmOrProvider String> f4 = new MyClass4<>();
+        @AllowedAlgorithms({"a"})
+        String x = f4.key;
+        @AllowedAlgorithms({"b"})
+        // ::error: (assignment.type.incompatible)
+        String y = f4.key;
+    }
+}
