56e7b8d2316b40d04f0b9cbc1c76fba0485df696
Werner Dietl <wdietl@gmail.com>
2025-03-18 21:39:27 -0400

    Adapt to Checker Framework change

--- src/main/java/org/checkerframework/checker/crypto/CryptoChecker.java
+++ src/main/java/org/checkerframework/checker/crypto/CryptoChecker.java
@@ -2,6 +2,7 @@ package org.checkerframework.checker.crypto;
 
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.common.value.ValueChecker;
+import org.checkerframework.framework.source.SourceChecker;
 import org.checkerframework.framework.source.SupportedLintOptions;
 
 import java.util.Set;
@@ -10,8 +11,8 @@ import java.util.Set;
 public class CryptoChecker extends BaseTypeChecker {
 
     @Override
-    protected Set<Class<? extends BaseTypeChecker>> getImmediateSubcheckerClasses() {
-        Set<Class<? extends BaseTypeChecker>> checkers = super.getImmediateSubcheckerClasses();
+    protected Set<Class<? extends SourceChecker>> getImmediateSubcheckerClasses() {
+        Set<Class<? extends SourceChecker>> checkers = super.getImmediateSubcheckerClasses();
         checkers.add(ValueChecker.class);
         return checkers;
     }
